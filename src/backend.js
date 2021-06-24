7a624a060f4077ad08a2c13777ce1bf8d126ebfe
Shreyas <shreyxs@gmail.com>
2021-06-25 00:05:51 +0530

    changed backend URL

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,4 @@
-export const API = "https://shreyas-smart-brain-api.herokuapp.com";
+// export const API = "https://shreyas-smart-brain-api.herokuapp.com";
+export const API = "https://smart-brain-api-2.herokuapp.com";
 // export const API = "http://192.168.0.158:5000";
 // export const API = "http://localhost:5000";
